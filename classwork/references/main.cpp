0860931e3e78ff6ab42343ead42c21421654b66c
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-09 11:12:36 -0700

    Create basic arrays for c and c++ styles

--- classwork/references/main.cpp
+++ classwork/references/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <array>
 
-int main(int, char**){
+int main(int, char **)
+{
     std::cout << "Hello, from references!\n";
+
+    // C-style array
+    // int cArr[] = {1, 2, 3};
+    int cArr[]{1, 2, 3};
+
+    // C++ class for array
+    std::array<int, 3> sArr{1, 2, 3};
 }
