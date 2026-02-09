5d17a1760b93d3123ba7224e567f26388920e601
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-08 20:54:12 -0700

    Modify code to loop until told

--- assignments/credit-card-validation/main.cpp
+++ assignments/credit-card-validation/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "creditCardValidation.cpp"
 
-int main(int, char**){
-    std::cout << "Hello, from CreditCardValidation!\n";
+int main(int, char **)
+{
+    long int ccNumber;
+    char repeat = 'y';
+    std::string holder = "";
+    std::cout << "\nHello, from CreditCardValidation!" << std::endl;
+    do
+    {
+        std::cout << "\nPlease enter a credit card number to validate: ";
+        std::cin >> ccNumber;
+        isValid(ccNumber) ? std::cout << "\nThe number is valid." << std::endl : std::cout << "The number is invalid." << std::endl;
+        std::cout << "Would you like to verify another card?\nEnter \"Y\" or \"N\"" << std::endl;
+        std::cin >> holder;
+        repeat = std::tolower(holder.at(0));
+    } while (repeat != 'n');
 }
