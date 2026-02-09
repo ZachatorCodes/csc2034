77583772990f583c1f2b01f295ff072f1b83e705
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-08 21:14:50 -0700

    Change formatting

--- assignments/credit-card-validation/main.cpp
+++ assignments/credit-card-validation/main.cpp
@@ -12,7 +12,7 @@ int main(int, char **)
         std::cout << "\nPlease enter a credit card number to validate: ";
         std::cin >> ccNumber;
         isValid(ccNumber) ? std::cout << "\nThe number is valid." << std::endl : std::cout << "The number is invalid." << std::endl;
-        std::cout << "Would you like to verify another card?\nEnter \"Y\" or \"N\"" << std::endl;
+        std::cout << "\nWould you like to verify another card?\nEnter \"Y\" or \"N\"" << std::endl;
         std::cin >> holder;
         repeat = std::tolower(holder.at(0));
     } while (repeat != 'n');
