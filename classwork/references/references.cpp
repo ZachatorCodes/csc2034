59cd3be8865710d8a667c12edb7f5c09f6e5b874
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-11 12:13:26 -0700

    Add stuff

--- classwork/references/references.cpp
+++ classwork/references/references.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <array>
 
+void print(int a)
+{
+    std::cout << "Value: " << a << std::endl;
+}
+
 int main(int, char **)
 {
     int x{4}; // int x = 4;
-
+    
     int &y{x}; // int& y = x;
 
     std::cout << x << ' ' << y << std::endl;
@@ -12,4 +17,8 @@ int main(int, char **)
     x = 8;
 
     std::cout << x << ' ' << y << std::endl;
+
+    int z = 98;
+
+    print(z);
 }
