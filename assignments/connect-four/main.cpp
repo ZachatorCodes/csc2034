e04983f7ac8c41a54db3a114c9e4150f0a52a985
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-15 18:25:29 -0700

    Create basic template std::array and a template defined function to print said array

--- assignments/connect-four/main.cpp
+++ assignments/connect-four/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
+#include <array>
 
-int main(int, char**){
-    std::cout << "Hello, from ConnectFour!\n";
+// Template for 2D std::array (not C-style)
+template <typename T, std::size_t Row, std::size_t Col>
+using Array2D = std::array<std::array<T, Col>, Row>;
+
+// Prototype for function with template parameter
+template <typename T, std::size_t Row, std::size_t Col>
+void print2DArray(const Array2D<T, Row, Col> &arr);
+
+int main(int, char **)
+{
+    Array2D<char, 6, 7> board{{
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    }};
+
+    print2DArray(board);
+}
+
+// Template parameter function
+template <typename T, std::size_t Row, std::size_t Col>
+void print2DArray(const Array2D<T, Row, Col> &arr)
+{
+    std::cout << '\n';
+    for (const auto &row : arr)
+    {
+        for (const auto &element : row)
+        {
+            std::cout << "|" << element;
+        }
+        std::cout << "|";
+        std::cout << '\n';
+    }
 }
