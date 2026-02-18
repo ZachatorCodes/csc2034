4034453fad81b24686959f511a49012b69599284
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-18 12:11:25 -0700

    Add pointer and references

--- classwork/dynamic-memory/main.cpp
+++ classwork/dynamic-memory/main.cpp
@@ -15,27 +15,34 @@ struct IArray
 
 int main(int, char **)
 {
-    std::cout << "Hello World!" << std::endl;
+    // std::cout << "Hello World!" << std::endl;
 
-    int arr[] = {1, 2, 3, 4, 5};
+    // int arr[] = {1, 2, 3, 4, 5};
 
-    size_t length = 5;
-    int *dArr1 = new int[1000000]{};
-    dArr1[0] = 3;
+    // size_t length = 5;
+    // int *dArr1 = new int[1000000]{};
+    // dArr1[0] = 3;
 
-    delete[] dArr1; // Must delete variables created with 'new' or 'new[]'
-    for (int i = 0; i < 4; i++)
-    {
-        foo();
-    }
+    // delete[] dArr1; // Must delete variables created with 'new' or 'new[]'
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     foo();
+    // }
 
-    int bigArr[1000]{};
+    // int bigArr[1000]{};
 
-    // Some simple stuff with main
-    int arr1[] = {1, 2, 3};
-    IArray arr2 = {arr1, 3};
+    // // Some simple stuff with main
+    // int arr1[] = {1, 2, 3};
+    // IArray arr2 = {arr1, 3};
 
-    IArray arr3{new int[]{1, 2, 3}, 3};
-    IArray arr4 = {new int[]{1, 2, 3}, 3}; // SAME
-    delete[] arr4.array;
+    // IArray arr3{new int[]{1, 2, 3}, 3};
+    // IArray arr4 = {new int[]{1, 2, 3}, 3}; // SAME
+    // delete[] arr4.array;
+
+    int a = 8;
+    int* b = &a;
+    int &c = *b;
+    std::cout << "Address: " << b << std::endl;
+    std::cout << "Pointer: " << *b << std::endl;
+    std::cout << "Pointer: " << &c << std::endl;
 }
