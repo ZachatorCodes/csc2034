e1d8afa92b3aa0a6c01cc0a5c77bba9821601a84
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-18 11:10:37 -0700

    Delete comment and format

--- classwork/dynamic-memory/main.cpp
+++ classwork/dynamic-memory/main.cpp
@@ -1,15 +1,7 @@
-/**
- * @file Classwork/DynamicMemory/main.cpp
- * @brief Classwork 2/16.
- *
- * Dealing with dynamic memory.
- * - What's the lifetime of the data.
- */
-
 #include "foo.cpp"
 #include <iterator>
 
-int gArr[] = { 1, 3, 0, 4, 5 };
+int gArr[] = {1, 3, 0, 4, 5};
 
 /**
  * @class IArray
@@ -17,22 +9,22 @@ int gArr[] = { 1, 3, 0, 4, 5 };
  */
 struct IArray
 {
-    int* array;
+    int *array;
     size_t size;
 };
 
-int main( int, char** )
+int main(int, char **)
 {
     std::cout << "Hello World!" << std::endl;
 
-    int arr[] = { 1, 2, 3, 4, 5 };
+    int arr[] = {1, 2, 3, 4, 5};
 
     size_t length = 5;
-    int* dArr1 = new int[1000000]{};
+    int *dArr1 = new int[1000000]{};
     dArr1[0] = 3;
 
     delete[] dArr1; // Must delete variables created with 'new' or 'new[]'
-    for ( int i = 0; i < 4; i++ )
+    for (int i = 0; i < 4; i++)
     {
         foo();
     }
@@ -40,10 +32,10 @@ int main( int, char** )
     int bigArr[1000]{};
 
     // Some simple stuff with main
-    int arr1[] = { 1, 2, 3 };
-    IArray arr2 = { arr1, 3 };
+    int arr1[] = {1, 2, 3};
+    IArray arr2 = {arr1, 3};
 
-    IArray arr3{ new int[]{ 1, 2, 3 }, 3 };
-    IArray arr4 = { new int[]{ 1, 2, 3 }, 3 }; // SAME
+    IArray arr3{new int[]{1, 2, 3}, 3};
+    IArray arr4 = {new int[]{1, 2, 3}, 3}; // SAME
     delete[] arr4.array;
 }
