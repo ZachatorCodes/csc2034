e3dabee553d393b0e9e36964a71ebe38950f1b3a
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-20 11:06:53 -0700

    Write function to take in a user number, increase vector size, add the user number to the vector, and loop until the user enters a negative number.

--- exams/exam-1/median.cpp
+++ exams/exam-1/median.cpp
@@ -8,5 +8,17 @@ void findMedian();
 // Function Definitions
 void collectData(std::vector<double> &nums)
 {
-    std::cout << "Test Print";
+    double userNum;
+    int counter = 0;
+    do
+    {
+        std::cout << "Please enter a number: ";
+        std::cin >> userNum;
+        if (userNum > -1)
+        {
+            nums.resize(nums.size() + 1);
+            nums[counter] = userNum;
+            counter++;
+        }
+    } while (userNum > -1);
 }
