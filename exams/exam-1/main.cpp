ff8c900f7f3165674066afed11a5dadf4a21c9a9
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-20 11:07:21 -0700

    Write basic user info

--- exams/exam-1/main.cpp
+++ exams/exam-1/main.cpp
@@ -3,6 +3,12 @@
 
 int main(int, char **)
 {
-    std::cout << "Hello, from exam-1!\n";
-    collectData();
+    // provide user with basic program functionality information
+    std::cout << "Welcome to the Zach's median finder application!" << '\n'
+              << "You can enter as many positive numbers as you would like." << '\n'
+              << "To end data collection, enter any negative number." << '\n'
+              << std::endl;
+
+    std::vector<double> nums{}; // initialize empty vector
+    collectData(nums);          // collect data method from median.cpp file
 }
