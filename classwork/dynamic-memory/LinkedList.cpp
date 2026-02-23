7167878ad244fc5ae4b3a1a929731369343a98b1
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-23 12:07:25 -0700

    Create basic linked list with destructor methods

--- classwork/dynamic-memory/LinkedList.cpp
+++ classwork/dynamic-memory/LinkedList.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
 
-int main(int, char**)
+struct Data
 {
-    std::cout << "Hello World!" << std::endl;
+    int number;
+};
+
+struct Node
+{
+    Data *data;
+    Node *next;
+
+    ~Node()
+    {
+        delete data;
+        std::cout << "Deleted data" << std::endl;
+        std::cout << "Deleted node" << std::endl;
+    }
+};
+
+struct LinkedList
+{
+    Node *head = nullptr;
+    size_t count = 0;
+
+    ~LinkedList()
+    {
+        Node *node = head;
+        while (node)
+        {
+            Node *restOfList = node->next;
+            delete node;
+            node = restOfList;
+        };
+    }
+};
+
+LinkedList createList(int numElems)
+{
+    LinkedList ls{nullptr, 0};
+    srand(time(nullptr));
+    for (size_t i = 0; i < numElems; i++)
+    {
+        Node *node = new Node{nullptr, nullptr};
+        Data *data = new Data{rand()};
+        node->data = data;
+        node->next = ls.head;
+        ls.head = node;
+        ls.count++;
+    }
+    return ls;
+}
+
+int main(int, char **)
+{
+    LinkedList list = createList(3);
+
+    Node *node = list.head;
+    while (node != nullptr)
+    {
+        std::cout << "Data: " << node->data->number << std::endl;
+        node = node->next;
+    }
 }
