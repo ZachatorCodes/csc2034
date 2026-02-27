111b2f120b939fca473369abbdd0a844117e1f45
ZachatorCodes <zachatorcodes@gmail.com>
2026-02-27 11:56:10 -0700

    Test creation

--- classwork/linked-list/main.cpp
+++ classwork/linked-list/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "IntList.h"
 
-int main(int, char**){
+void test()
+{
+    IntList seq1, seq2;
+
+    seq1.insert(10);
+    seq1.insert(-2);
+    seq1.insert(8);
+
+    seq1.print();
+}
+
+int main(int, char **)
+{
     std::cout << "Hello, from linked-list!\n";
+    test();
 }
