9a2dca925b8ee5b0b04cc10da8bfa84edccc6c18
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-02 11:56:27 -0700

    Show shallow copy vs hard copy

--- classwork/linked-list/main.cpp
+++ classwork/linked-list/main.cpp
@@ -3,15 +3,21 @@
 
 void test()
 {
-    IntList seq1;
+    IntList seq1; // create list
 
     seq1.insert(10);
     seq1.insert(-2);
     seq1.insert(8);
-
     seq1.print();
 
-    IntList seq2 {seq1};
+    IntList seq2{seq1}; // SHALLOW COPY
+
+    seq2.print();
+    seq2.insert(23);
+    std::cout << "After insert" << std::endl;
+    seq2.print();
+    seq1.print();
+    std::cout << seq2.size() << " " << seq1.size() << std::endl;
 }
 
 int main(int, char **)
