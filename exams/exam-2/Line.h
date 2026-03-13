64f2263396d9eac3b4e093a0d50ae42931ead735
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-13 12:06:14 -0600

    Add prototypes for copy and assignment constructors

--- exams/exam-2/Line.h
+++ exams/exam-2/Line.h
@@ -10,6 +10,8 @@ class Line
 public:
     Line(int x1, int y1, int x2, int y2);         // constructor
     ~Line();                                      // destructor
+    Line(const Line &other);                      // copy constructor
+    Line &operator=(const Line &other);           // assignment constructor
     Point *getPt1();                              // returns pointer to point 1
     Point *getPt2();                              // returns pointer to point 2
     double calculateLength(Point *p1, Point *p2); // takes in two pointers to calculate length
