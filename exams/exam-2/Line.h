40c64d1c7505d0ddfe8e257651da3debbfdb9126
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-13 12:05:06 -0600

    Add prototypes

--- exams/exam-2/Line.h
+++ exams/exam-2/Line.h
@@ -2,10 +2,15 @@
 
 #include "Point.h"
 
-class Line {
-    Point* pt1;
-    Point* pt2;
+class Line
+{
+    Point *pt1;
+    Point *pt2;
 
 public:
-    Line::Line(int x1, int y1, int x2, int y2); 
+    Line(int x1, int y1, int x2, int y2);         // constructor
+    ~Line();                                      // destructor
+    Point *getPt1();                              // returns pointer to point 1
+    Point *getPt2();                              // returns pointer to point 2
+    double calculateLength(Point *p1, Point *p2); // takes in two pointers to calculate length
 };
