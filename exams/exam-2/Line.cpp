17dfcd7fd138d4f8a0255e1e2d9c644ec8f8e04e
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-13 12:58:17 -0600

    Change function to match prototype and call in main

--- exams/exam-2/Line.cpp
+++ exams/exam-2/Line.cpp
@@ -40,12 +40,24 @@ Point *Line::getPt2()
     return pt2;
 }
 
-double Line::calculateLength(Point *point1, Point *point2)
+double Line::slope()
 {
-    int p1X = point1->getX();
-    int p1Y = point1->getY();
-    int p2X = point2->getX();
-    int p2Y = point2->getY();
+    int p1X = this->getPt1()->getX();
+    int p1Y = this->getPt1()->getY();
+    int p2X = this->getPt2()->getX();
+    int p2Y = this->getPt2()->getY();
+
+    double slope = ((p2Y - p1Y) / (p2X - p1X));
+
+    return slope;
+}
+
+double Line::segmentLength()
+{
+    int p1X = this->getPt1()->getX();
+    int p1Y = this->getPt1()->getY();
+    int p2X = this->getPt2()->getX();
+    int p2Y = this->getPt2()->getY();
 
     double length = std::sqrt(std::pow((p2X - p1X), 2.0) + (std::pow((p2Y - p1Y), 2.0)));
 
