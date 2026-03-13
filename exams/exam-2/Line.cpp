49b79d00cc56cfa8a05981e8c1e50bba17adbc9e
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-13 12:02:51 -0600

    Add space (formatting)

--- exams/exam-2/Line.cpp
+++ exams/exam-2/Line.cpp
@@ -2,10 +2,9 @@
 #include <tuple>
 #include "Line.h"
 
-
 Line::Line(int x1, int y1, int x2, int y2)
 {
-    pt1 = new Point{x1,y1};
-    pt2 = new Point{x2,y2};
-} 
+    pt1 = new Point{x1, y1};
+    pt2 = new Point{x2, y2};
+}
 
