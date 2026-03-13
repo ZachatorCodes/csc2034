c0127fd1c4138dc43dd4b3543ff88e10f5f130f4
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-13 12:01:31 -0600

    Implement getter and setter definitions for point

--- exams/exam-2/Point.cpp
+++ exams/exam-2/Point.cpp
@@ -1,6 +1,26 @@
 #include "Point.h"
 
 Point::Point(int x, int y)
-    : m_x(x)
-    , m_y(y)
-{}
+    : m_x(x), m_y(y)
+{
+}
+
+void Point::setX(int val)
+{
+    m_x = val;
+}
+
+void Point::setY(int val)
+{
+    m_y = val;
+}
+
+int Point::getX()
+{
+    return m_x;
+}
+
+int Point::getY()
+{
+    return m_y;
+}
