3b059a14b19d1156f1f05dd077f1ba2c36ec36be
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-23 09:41:33 -0600

    Modify main to create different shapes using inheritence

--- classwork/inheritance/main.cpp
+++ classwork/inheritance/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "Shape.h"
+#include "Circle.h"
+#include "Rectangle.h"
 
 int main(int, char**){
-    std::cout << "Hello, from inheritance!\n";
+    std::cout << "Hello, from Inheritance!\n";
+
+    Shape shape1{"Red", true};
+
+    std::cout << shape1;
+
+    Circle circle1{"Green", true, 2.0};
+
+    std::cout << circle1;
+
+    Rectangle rect1("Blue", false, 2, 3);
+    std::cout << "Rect Color: " << rect1.getColor() << std::endl;
+    std::cout << rect1;
 }
