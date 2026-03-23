73b5b3efaab7ca9ac7f81bf424392eb4fb1d9286
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-23 11:49:45 -0600

    Add notes

--- classwork/pointers-exercise/Inventory.h
+++ classwork/pointers-exercise/Inventory.h
@@ -2,11 +2,16 @@
 #include <vector> // goes above header file (convention, doesn't make a difference)
 #include "Car.h"
 
-// Inventory is a pointer to a vector of pointers to cars | Car* (IS A POINTER)
 // Whenever you use a pointer, you must use *
 // All addresses are 8 bytes (on a 64-bit machine)
 
+// The moment you need a destructor, you probably need an assignment operator
+
+// Inventory is a pointer to a vector of pointers to cars | Car* (IS A POINTER)
 class Inventory
 {
-    std::vector<Car *> cars;
+    std::vector<Car *> *cars;
+
+public:
+    Inventory();
 };
