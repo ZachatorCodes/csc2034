111daa668a8c41ba1acc76db2cc34ee53fa6407c
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-25 11:24:33 -0600

    Make new car

--- classwork/pointers-exercise/main.cpp
+++ classwork/pointers-exercise/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "Inventory.h"
 
-int main(int, char**){
+int main(int, char **)
+{
     std::cout << "Hello, from pointers-exercise!\n";
 
-    Inventory* dealerStock = new Inventory();
+    Inventory *dealerStock = new Inventory();
+
+    Car *gmc = new Car{"GMC", "Sierra", "GMCSIERRA1234"};
 }
