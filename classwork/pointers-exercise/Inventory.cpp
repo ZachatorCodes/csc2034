c4541beffb95bf3e28495c4893528795c0cf361c
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-27 11:40:50 -0600

    Change destructory to be DRY

--- classwork/pointers-exercise/Inventory.cpp
+++ classwork/pointers-exercise/Inventory.cpp
@@ -20,15 +20,7 @@ Inventory::Inventory(Inventory& other) : Inventory()
 
 Inventory::~Inventory()
 {
-    auto it = cars->begin(); // cars points to the vector, get the beginning of the list using std library
-    // auto it = (*cars).begin();
-    // ^^ it is short for iterator
-
-    while (it != cars->end())
-    {
-        delete *it; // delete auto dereferences (same as ** kind of)
-        it++;
-    }
+   clearInventory();
 }
 
 bool Inventory::addToInventory(Car* car)
