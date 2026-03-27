bef7de3d6f5ddc90896c803fd8c3c73a3adaf26f
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-27 11:11:12 -0600

    Format with new clang file

--- classwork/pointers-exercise/main.cpp
+++ classwork/pointers-exercise/main.cpp
@@ -1,25 +1,25 @@
 #include "Inventory.h"
 #include <iostream>
 
-int main(int, char **)
+int main(int, char**)
 {
     std::cout << "Hello, from pointers-exercise!\n";
 
-    Inventory *dealerStock = new Inventory();
+    Inventory* dealerStock = new Inventory();
     Inventory d3{};
     d3 = *dealerStock;
 
-    Car *gmc = new Car{"GMC", "Sierra", "GMCSIERRA1234"};
+    Car* gmc = new Car{"GMC", "Sierra", "GMCSIERRA1234"};
     dealerStock->addToInventory(gmc);
     // OR (*dealerStock).addToInventory(gmc);
 
-    Car *toyota = new Car{"Toyota", "4Runner", "TOYOTA4RUNNER1234"};
-    Car *honda = new Car{"Honda", "Accord", "HONDAACCORD1234"};
+    Car* toyota = new Car{"Toyota", "4Runner", "TOYOTA4RUNNER1234"};
+    Car* honda = new Car{"Honda", "Accord", "HONDAACCORD1234"};
     dealerStock->addToInventory(toyota);
     (*dealerStock).addToInventory(honda);
 
     Inventory d2{*dealerStock};
-    Inventory *dealerStock2 = &d2; // &d2 reads as address of d2
+    Inventory* dealerStock2 = &d2; // &d2 reads as address of d2
 
     std::cout << dealerStock;
     std::cout << dealerStock2;
