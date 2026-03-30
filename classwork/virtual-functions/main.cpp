749c97f6ec68aa9937554045d57a9e1441b982a4
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-30 11:43:07 -0600

    Test all classes and method and type casting

--- classwork/virtual-functions/main.cpp
+++ classwork/virtual-functions/main.cpp
@@ -6,6 +6,17 @@ int main(int, char**)
 {
     std::cout << "Hello, from virtual-functions!\n";
 
-    Animal* anim = new Animal("Billy", "Orange", 3.2);
-    Cat* cat1 = new Cat("Tabby", "Orange", 25.4, 4.5);
+    Animal* anim = new Animal("Billy", "Orange", 3.2); // variable holds pointer to animal
+    std::cout << anim->makeSound() << std::endl;
+
+    Cat* cat1 = new Cat("Tabby", "Orange", 25.4, 4.5); // variable holds pointer to cat
+    std::cout << cat1->makeSound() << std::endl;
+
+    std::cout << dynamic_cast<Animal*>(cat1)->makeSound() << std::endl; // dynamic cast for pointers
+
+    Animal* anim1 = cat1;
+    std::cout << anim1->makeSound() << std::endl; // uses type casting and uses parent method
+    std::cout << anim1->makeProperSound() << std::endl; // overrides
+
+
 }
