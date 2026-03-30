e645984f338ba4360da198d3e93b95a97b28103b
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-30 11:20:12 -0600

    Import header and string and make constructor

--- classwork/virtual-functions/Cat.h
+++ classwork/virtual-functions/Cat.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "Animal.h"
+#include <string>
 
-class Cat
+class Cat : public Animal
 {
-    
+    double m_whiskerLength{2.0}; // better way to initialize
+
+  public:
+    Cat(std::string name, std::string color, double weight, double whiskerLength);
 };
