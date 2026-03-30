712d59c31261a516ddefe053b9c7fbaef3631a53
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-30 11:25:26 -0600

    Implement initializer list and getter for whisker length

--- classwork/virtual-functions/Cat.cpp
+++ classwork/virtual-functions/Cat.cpp
@@ -1,5 +1,11 @@
 #include "Cat.h"
 
 Cat::Cat(std::string name, std::string color, double weight, double whiskerLength)
+    : Animal(name, color, weight), m_whiskerLength(whiskerLength) // initializer list
 {
+}
+
+double Cat::getWhiskerLength() const
+{
+    return m_whiskerLength;
 }
