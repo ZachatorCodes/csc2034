3da794f77273e950af65ce882a377c438275c10a
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-30 11:19:21 -0600

    Import header and format

--- classwork/virtual-functions/main.cpp
+++ classwork/virtual-functions/main.cpp
@@ -1,6 +1,8 @@
+#include "Animal.h"
 #include <iostream>
 
-int main(int, char**){
+int main(int, char**)
+{
     std::cout << "Hello, from virtual-functions!\n";
 
     Animal* anim = new Animal("Billy", "Orange", 3.2);
