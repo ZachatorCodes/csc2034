4db3759363836929b06f1b5ef5031faa38de290e
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-30 12:13:10 -0600

    Write default constructor and destructor

--- classwork/virtual-functions/Cat.cpp
+++ classwork/virtual-functions/Cat.cpp
@@ -1,8 +1,19 @@
 #include "Cat.h"
 
+Cat::Cat()
+{
+    m_whiskerLength = 1.0;
+}
+
 Cat::Cat(std::string name, std::string color, double weight, double whiskerLength)
     : Animal(name, color, weight), m_whiskerLength(whiskerLength) // initializer list
 {
+}
+
+Cat::~Cat()
+{
+    std::cout << "Deleting cat aliases" << std::endl;
+    delete catAliases;
 }
 
 double Cat::getWhiskerLength() const
