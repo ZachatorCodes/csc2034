91906502d3d2661ee8c87cf68e0c0b676507d67d
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-30 11:25:35 -0600

    Make cat and modify imports

--- classwork/virtual-functions/main.cpp
+++ classwork/virtual-functions/main.cpp
@@ -1,4 +1,5 @@
-#include "Animal.h"
+// #include "Animal.h" (not needed because Cat.h includes Animal.h)
+#include "Cat.h"
 #include <iostream>
 
 int main(int, char**)
@@ -6,4 +7,5 @@ int main(int, char**)
     std::cout << "Hello, from virtual-functions!\n";
 
     Animal* anim = new Animal("Billy", "Orange", 3.2);
+    Cat* cat1 = new Cat("Tabby", "Orange", 25.4, 4.5);
 }
