f13f86d81ae71d8f6f59f4d5511b04db150258e6
ZachatorCodes <zachatorcodes@gmail.com>
2026-03-30 12:13:15 -0600

    Add suff

--- classwork/virtual-functions/Cat.h
+++ classwork/virtual-functions/Cat.h
@@ -4,9 +4,12 @@
 class Cat : public Animal // extends
 {
     double m_whiskerLength{2.0}; // better way to initialize
+    std::string* catAliases;
 
   public:
+    Cat();
     Cat(std::string name, std::string color, double weight, double whiskerLength);
+    ~Cat() override;
     double getWhiskerLength() const; // does not modify data, good habit for getters
     std::string makeSound();
     std::string makeProperSound() override; // overrides virtual function
