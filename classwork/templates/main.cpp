3b060d54afd147b1e9625673614ec6a2b03d803e
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-08 11:26:47 -0600

    Experiment with templates and overloading

--- classwork/templates/main.cpp
+++ classwork/templates/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from templates!\n";
+// int add(int a, int b)
+// {
+//     return a + b;
+// }
+
+// double add(double a, double b)
+// {
+//     return a + b;
+// }
+
+template <typename T> T add(T a, T b)
+{
+    return a + b;
+}
+
+// this template returns whatever the second parameter is, in this case V
+template <typename T, typename V> V add(T a, V b)
+{
+    return a + b;
+}
+
+int main(int, char**)
+{
+    std::cout << add(2, 3) << std::endl;
+
+    std::cout << add(2.5, 3.7) << std::endl; // heads up, no warning if not overloaded
+
+    std::cout << add(2, 3.7) << std::endl;
+
+    std::cout << add(3.7, 2) << std::endl;
 }
