8e10e6b72332c55e1669e2df48c8b9886d9e3a7f
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-11 13:15:42 -0600

    Write prototype and implement method to write header for file

--- assignments/ranked-choice-voting/main.cpp
+++ assignments/ranked-choice-voting/main.cpp
@@ -6,6 +6,7 @@
 
 // PROTOTYPES
 int readData(const std::string filepath, std::vector<std::vector<std::string>>& data);
+int writeData(const std::string filepath, const std::vector<std::vector<std::string>>& data, int iteration);
 
 // FUNCTION DEFINITIONS
 int readData(const std::string filepath, std::vector<std::vector<std::string>>& data)
@@ -41,22 +42,54 @@ int readData(const std::string filepath, std::vector<std::vector<std::string>>& 
     file.close(); // Close the file
 
     // Optional: Print the read data to verify
-    for (const auto& row : data)
+    // for (const auto& row : data)
+    // {
+    //     for (const auto& field : row)
+    //     {
+    //         std::cout << field << "\t";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    return 0;
+}
+
+int writeData(const std::string filepath, const std::vector<std::vector<std::string>>& data, int iteration)
+{
+    std::ofstream file(filepath); // open the file in output mode
+
+    // check if the file was opened successfully
+    if (!file.is_open())
     {
-        for (const auto& field : row)
-        {
-            std::cout << field << "\t";
-        }
-        std::cout << std::endl;
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
     }
 
+    // Write the header for the file
+    file << "Canidates,";
+    for (int i = 0; i < data[0].size(); i++)
+    {
+        if (i == data[0].size() - 1)
+        {
+            file << data[0][i] << '\n';
+        }
+        else
+        {
+            file << data[0][i] << ',';
+        }
+    }
+
+    // close the file
+    file.close();
     return 0;
 }
 
 int main(int, char**)
 {
-    std::string filepath = "../../../CandidateVotes.csv"; // original path to the CSV file
-    std::vector<std::vector<std::string>> data;           // vector to store the read data
+    std::string initialVotes = "../../../CandidateVotes.csv"; // original path to the CSV file
+    std::string results = "../../../ElectionResults.csv";     // file path for tallied results
+    std::vector<std::vector<std::string>> data;               // vector to store the read data
 
-    readData(filepath, data); // code runs in build directory, so path is relative to that
+    readData(initialVotes, data); // code runs in build directory, so path is relative to that
+    writeData(results, data, 0);
 }
