d800dae8f3d8cfadeeae7a037f72713cdb23178e
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-17 11:26:28 -0600

    Add setters for all member variables

--- exams/exam-2-makeup/Address.cpp
+++ exams/exam-2-makeup/Address.cpp
@@ -6,3 +6,18 @@ Address::Address(std::string street, std::string city, std::string state)
     m_city = city;
     m_state = state;
 }
+
+void Address::setCity(std::string city)
+{
+    m_city = city;
+}
+
+void Address::setState(std::string state)
+{
+    m_state = state;
+}
+
+void Address::setStreet(std::string street)
+{
+    m_street = street;
+}
