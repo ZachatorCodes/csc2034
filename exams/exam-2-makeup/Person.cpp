0763b8fd00409ae9a9490a43e6a90d2dbf10a90b
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-17 12:52:32 -0600

    Change to use address overload

--- exams/exam-2-makeup/Person.cpp
+++ exams/exam-2-makeup/Person.cpp
@@ -53,7 +53,6 @@ void Person::setAddress(Address* addy)
 
 std::ostream& operator<<(std::ostream& out, Person person)
 {
-    out << person.m_name << ", " << person.m_addr->getStreet() << ", " << person.m_addr->getCity() << ", "
-        << person.m_addr->getState();
+    out << person.m_name << ", " << *(person.m_addr);
     return out;
 }
