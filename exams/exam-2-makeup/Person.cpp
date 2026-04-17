e981c207206a04b6cd44096a64cf8f6f7ace5485
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-17 11:26:57 -0600

    Implement setName and getAddress to dereference

--- exams/exam-2-makeup/Person.cpp
+++ exams/exam-2-makeup/Person.cpp
@@ -1,6 +1,17 @@
 #include "Person.h"
+#include "Address.h"
 
 Person::Person(std::string name)
 {
     m_name = name;
+}
+
+void Person::setName(std::string name)
+{
+    m_name = name;
+}
+
+Address Person::getAddress()
+{
+    return (*m_addr);
 }
