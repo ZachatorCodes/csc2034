b188b767359401eb0350f099e7934e956a7e49ad
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-18 10:36:29 -0600

    Implement constructors

---
+++ assignments/school-records/Person.cpp
@@ -0,0 +1,11 @@
+#include "Person.h"
+
+Person::Person()
+    : m_name(""), m_address(""), m_phoneNumber(""), m_emailAddress("")
+{
+}
+
+Person::Person(std::string name, std::string address, std::string phoneNumber, std::string emailAddress)
+    : m_name(name), m_address(address), m_phoneNumber(phoneNumber), m_emailAddress(emailAddress)
+{
+}
