74a5d0468d66197c0d83e1ab430d3f1e7fa5a02d
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-18 12:31:30 -0600

    Implement default and parameterized constructor for Student class

---
+++ assignments/school-records/Student.cpp
@@ -0,0 +1,11 @@
+#include "Student.h"
+
+Student::Student() : Person(), m_status(Freshman)
+{
+}
+
+Student::Student(std::string name, std::string address, std::string phoneNumber, std::string emailAddress,
+                 enum status m_status)
+    : Person(name, address, phoneNumber, emailAddress), m_status(m_status)
+{
+}
