4c5c91cb09d51756b5636d6571b38801fe795d3e
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-30 17:40:30 -0600

    Write function to write basic person

--- assignments/school-records/Database.cpp
+++ assignments/school-records/Database.cpp
@@ -1,3 +1,12 @@
 #include "Database.h"
 
 Database::Database() : m_file(m_filePath) {};
+
+bool Database::writePerson(Person* person)
+{
+    m_file << '\"' << person->getName() << "\",\"" << person->getAddress() << "\",\"" << person->getPhoneNumber()
+           << "\",\"" << person->getEmailAddress() << "\"\n";
+    m_file.close();
+
+    return true;
+}
