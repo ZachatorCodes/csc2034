a3447d414388aa201cb152b19795d22485ea7dd1
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-30 17:08:50 -0600

    Update status

--- assignments/school-records/Student.cpp
+++ assignments/school-records/Student.cpp
@@ -5,7 +5,7 @@ Student::Student() : Person(), m_status(Freshman)
 }
 
 Student::Student(std::string name, std::string address, std::string phoneNumber, std::string emailAddress,
-                 enum status m_status)
+                 status m_status)
     : Person(name, address, phoneNumber, emailAddress), m_status(m_status)
 {
 }
