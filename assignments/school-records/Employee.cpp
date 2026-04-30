ddc66abca139605af1000c7e5b10420c461ee654
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-30 17:10:49 -0600

    Add getters and setters

--- assignments/school-records/Employee.cpp
+++ assignments/school-records/Employee.cpp
@@ -9,3 +9,33 @@ Employee::Employee(std::string name, std::string address, std::string phoneNumbe
     : Person(name, address, phoneNumber, emailAddress), m_office(office), m_salary(salary), m_dateHired(dateHired)
 {
 }
+
+const std::string& Employee::getOffice() const
+{
+    return m_office;
+}
+
+const std::string& Employee::getDateHired() const
+{
+    return m_dateHired;
+}
+
+double Employee::getSalary() const
+{
+    return m_salary;
+}
+
+void Employee::setOffice(std::string office)
+{
+    m_office = office;
+}
+
+void Employee::setDateHired(std::string dateHired)
+{
+    m_dateHired = dateHired;
+}
+
+void Employee::setSalary(double salary)
+{
+    m_salary = salary;
+}
