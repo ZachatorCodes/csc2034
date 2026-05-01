49f2acf3e4f9df8c8ef9c4c8acbcc708171ca4ec
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-01 10:57:24 -0600

    Test casting ability

--- assignments/school-records/Database.cpp
+++ assignments/school-records/Database.cpp
@@ -5,9 +5,12 @@ Database::Database() : m_file(m_filePath) {};
 
 bool Database::writePerson(Person* person)
 {
-    m_file << '\"' << person->getName() << "\",\"" << person->getAddress() << "\",\"" << person->getPhoneNumber()
-           << "\",\"" << person->getEmailAddress() << "\"\n";
-    m_file.close();
+    Faculty* faculty = (dynamic_cast<Faculty*>(person));
+    std::cout << person->getClassName();
+    
+    // m_file << '\"' << person->getName() << "\",\"" << person->getAddress() << "\",\"" << person->getPhoneNumber()
+    //        << "\",\"" << person->getEmailAddress() << "\"\n";
+    // m_file.close();
 
     return true;
 }
