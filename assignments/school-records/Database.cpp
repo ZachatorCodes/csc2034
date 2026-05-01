a1ac5f59a898927ddcb6cc82a2206e8136084c03
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-01 12:06:10 -0600

    Change function to just print person object

--- assignments/school-records/Database.cpp
+++ assignments/school-records/Database.cpp
@@ -6,22 +6,6 @@ Database::Database() : m_file(m_filePath) {};
 
 bool Database::writePerson(Person* person)
 {
-    if (Faculty* faculty = dynamic_cast<Faculty*>(person))
-    {
-        std::cout << faculty->getOfficeHours() << std::endl;
-    }
-    else if (Staff* staff = dynamic_cast<Staff*>(person))
-    {
-        std::cout << staff->getClassName() << std::endl;
-    }
-    else if (Student* student = dynamic_cast<Student*>(person))
-    {
-        std::cout << student->getClassName() << std::endl;
-    }
-
-    // m_file << '\"' << person->getName() << "\",\"" << person->getAddress() << "\",\"" << person->getPhoneNumber()
-    //        << "\",\"" << person->getEmailAddress() << "\"\n";
-    // m_file.close();
-
+    m_file << *person << std::endl;
     return true;
 }
