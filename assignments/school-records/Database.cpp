11e16b20cd6526e59de57b2743d30999a357c059
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-01 11:00:37 -0600

    Add new line for print testing

--- assignments/school-records/Database.cpp
+++ assignments/school-records/Database.cpp
@@ -6,7 +6,7 @@ Database::Database() : m_file(m_filePath) {};
 bool Database::writePerson(Person* person)
 {
     Faculty* faculty = (dynamic_cast<Faculty*>(person));
-    std::cout << person->getClassName();
+    std::cout << person->getClassName() << std::endl;
 
     // m_file << '\"' << person->getName() << "\",\"" << person->getAddress() << "\",\"" << person->getPhoneNumber()
     //        << "\",\"" << person->getEmailAddress() << "\"\n";
