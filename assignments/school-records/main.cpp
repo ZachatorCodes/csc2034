b01a5fc52c632973f7e69b46600c0d45a47779ff
ZachatorCodes <zachatorcodes@gmail.com>
2026-04-30 18:10:56 -0600

    Add comment

--- assignments/school-records/main.cpp
+++ assignments/school-records/main.cpp
@@ -5,6 +5,7 @@
 
 int main(int, char**)
 {
+    // Create a new database object
     Database* database = new Database();
     database->writePerson(new Person("John Doe", "123 Main St", "555-1234", "john.doe@example.com"));
 }
