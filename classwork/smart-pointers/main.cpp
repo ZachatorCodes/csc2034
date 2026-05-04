020e3047c19ce0636f9cc5784f402d477de2d757
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-04 11:25:29 -0600

    Include more libraries

--- classwork/smart-pointers/main.cpp
+++ classwork/smart-pointers/main.cpp
@@ -1,4 +1,7 @@
-#include <iostream>
+#include "Person.h"
+#include <array>
+#include <memory> // needed for smart pointers
+#include <vector>
 
 int main(int, char**){
     std::cout << "Hello, from smart-pointers!\n";
