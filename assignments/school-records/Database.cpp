68748e8b792bab13334e11f5aa75699cd1cc72cd
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-03 18:39:23 -0600

    Implement readDatabase method

--- assignments/school-records/Database.cpp
+++ assignments/school-records/Database.cpp
@@ -10,3 +10,25 @@ bool Database::writePerson(Person* person)
     m_file << *person << std::endl;
     return true;
 }
+
+std::vector<std::vector<std::string>> Database::readDatabase()
+{
+    std::ifstream file(m_filePath);
+    std::string line;
+    std::vector<std::vector<std::string>> people;
+
+    while (std::getline(file, line))
+    {
+        std::stringstream ss(line); // Create a stringstream from the line
+        std::string field;
+        std::vector<std::string> row;
+
+        // Parse each field in the line using a comma delimiter
+        while (std::getline(ss, field, ','))
+        {
+            row.push_back(field);
+        }
+        people.push_back(row); // Add the row to the data storage
+    }
+    return people;
+}
