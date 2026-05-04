606ad77387532bd1fd29ea2be561ee342d4a0eb6
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-04 11:45:57 -0600

    Comment and write functions to print person

--- classwork/smart-pointers/main.cpp
+++ classwork/smart-pointers/main.cpp
@@ -1,18 +1,33 @@
 #include "Person.h"
 #include <array>
-#include <memory> // needed for smart pointers
 #include <vector>
+
+// Works with smart pointer
+void printPerson(std::unique_ptr<Person> person)
+{
+    std::cout << *person;
+}
+
+// Works with raw pointer
+void printPerson(Person* person)
+{
+    std::cout << *person;
+}
 
 int main(int, char**)
 {
-    // It is important to look at the lifetime of your data
-    // Use these to avoid C-style arrays
-    std::array<int, 5> arr{1, 2, 3, 4, 5};
-    std::vector<int> vec{1, 2, 3, 4, 5};
-
     // NOT the prefered way
     std::unique_ptr<Person> tom{new Person("Tom", 25)};
 
     // Prefered way
     auto mary{std::make_unique<Person>("Mary", 30)};
+
+    // Pointer is MOVED to printPerson()
+    // tom is now empty (ownership transferred)
+    printPerson(std::move(tom));
+
+    // Getting the raw pointer
+    printPerson(mary.get());
+
+    std::cout << "End of Program" << std::endl;
 }
