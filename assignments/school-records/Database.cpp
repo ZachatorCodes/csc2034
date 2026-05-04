d2393637978ec492d43a896ef6f423f3929d84b3
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-03 21:06:50 -0600

    If student, create student object to add

--- assignments/school-records/Database.cpp
+++ assignments/school-records/Database.cpp
@@ -28,6 +28,13 @@ std::vector<Person> Database::readDatabase()
         {
             personData.push_back(personField.substr(1, personField.size() - 2)); // Remove the quotes
         }
+
+        if (personData[0] == "Student")
+        {
+            Student student = Student(personData[1], personData[2], personData[3], personData[4], Status::Freshman);
+            student.setStatus(personData[5]);
+            people.push_back(student);
+        }
     }
     file.close();
 
