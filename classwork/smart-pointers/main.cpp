6eab2cec8a72fc0feb416cadddf4606bdfbb08ae
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-04 11:26:50 -0600

    Update comment and prefered method

--- classwork/smart-pointers/main.cpp
+++ classwork/smart-pointers/main.cpp
@@ -10,9 +10,9 @@ int main(int, char**)
     std::array<int, 5> arr{1, 2, 3, 4, 5};
     std::vector<int> vec{1, 2, 3, 4, 5};
 
-    // NOT the prefered way (exceptions can happen)
+    // NOT the prefered way
     std::unique_ptr<Person> tom{new Person("Tom", 25)};
 
     // Prefered way
-    std::unique_ptr<Person> mary{std::make_unique<Person>("Mary", 30)};
+    auto mary{std::make_unique<Person>("Mary", 30)};
 }
