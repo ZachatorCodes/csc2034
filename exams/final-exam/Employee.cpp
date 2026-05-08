02f77597c4899d13ff12c359d9818b29de62c824
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-08 12:08:27 -0600

    Prototype and implement functions

---
+++ exams/final-exam/Employee.cpp
@@ -0,0 +1,51 @@
+#include "Employee.h"
+
+Employee::Employee() : m_firstName("First Name"), m_lastName("Last Name"), m_ssNum("XXX-XX-XXXX")
+{
+}
+
+Employee::Employee(std::string fname, std::string lname, std::string ssnum)
+    : m_firstName(fname), m_lastName(lname), m_ssNum(ssnum)
+{
+}
+
+std::string Employee::getFirstName()
+{
+    return m_firstName;
+}
+
+std::string Employee::getLastName()
+{
+    return m_lastName;
+}
+
+std::string Employee::getSSNum()
+{
+    return m_ssNum;
+}
+
+void Employee::setFirstName(std::string fname)
+{
+    m_firstName = fname;
+}
+
+void Employee::setLastName(std::string lname)
+{
+    m_lastName = lname;
+}
+
+void Employee::setSSNum(std::string ssNum)
+{
+    m_ssNum = ssNum;
+}
+
+void Employee::print(std::ostream& out)
+{
+    out << "First Name: " << m_firstName << '\n' << "Last Name: " << m_lastName << '\n' << "SS Number: " << m_ssNum;
+}
+
+std::ostream& operator<<(std::ostream& out, Employee& emp)
+{
+    emp.print(out);
+    return out;
+}
---
+++ exams/final-exam/Employee.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Employee
+{
+    std::string m_firstName;
+    std::string m_lastName;
+    std::string m_ssNum;
+
+  public:
+    virtual double getPaymentAmount() = 0; // pure virtual function (abstract class)
+
+    Employee();
+    Employee(std::string fname, std::string lname, std::string ssnum);
+
+    std::string getFirstName();
+    std::string getLastName();
+    std::string getSSNum();
+
+    void setFirstName(std::string fname);
+    void setLastName(std::string lname);
+    void setSSNum(std::string ssNum);
+
+    virtual void print(std::ostream& out);
+    friend std::ostream& operator<<(std::ostream& out, Employee& emp);
+};
