30617f0624c1402323d2e2a1c0c117d60820580a
ZachatorCodes <zachatorcodes@gmail.com>
2026-05-08 12:37:21 -0600

    Create one new object for each class and print to test functionality

--- exams/final-exam/AccountsPayable.cpp
+++ exams/final-exam/AccountsPayable.cpp
@@ -1,12 +1,20 @@
+#include "CommissionEmployee.h"
 #include "Employee.h"
+#include "HourlyEmployee.h"
 #include "SalariedEmployee.h"
 #include <iostream>
 
 int main(int, char**)
 {
-    std::cout << "Hello, from AccountsPayable!\n";
-
     SalariedEmployee salEmp{SalariedEmployee("Zach", "Schwartz", "562756162", 1000.0)};
 
-    std::cout << salEmp;
+    std::cout << salEmp << std::endl;
+
+    CommissionEmployee comEmp{CommissionEmployee("Kishore", "Menezes", "54378928", 10.0, 32.0)};
+
+    std::cout << comEmp << std::endl;
+
+    HourlyEmployee hrEmp{HourlyEmployee("John", "Smith", "123456789", 22.0, 12.0)};
+
+    std::cout << hrEmp << std::endl;
 }
